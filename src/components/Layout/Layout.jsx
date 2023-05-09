4a8d5ae27ac465f85fac962ef78cf32fc87e0941
Olga Birkovska <aiba.aki9@gmail.com>
2023-05-09 21:05:58 +0300

    adding simple loader

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import { Outlet, NavLink } from "react-router-dom";
+import { Loader } from "components/Loader/Loader";
 import css from './Layout.module.css'
 
 const Layout = () => {
     return (
-        <main>
+        <>
             <header className={css.header}>
                 <nav className={css.nav}>
                     <NavLink to={'/cards'} style={{textDecoration: 'none'}}>
@@ -15,10 +16,12 @@ const Layout = () => {
                     </NavLink>
                 </nav>
             </header>
-            <Suspense fallback={'Shalala'}>
-                <Outlet />
-            </Suspense>
-        </main>
+            <main className={css.main}>
+                <Suspense fallback={<Loader />}>
+                    <Outlet />
+                </Suspense>
+            </main>
+        </>
     )
 }
 
